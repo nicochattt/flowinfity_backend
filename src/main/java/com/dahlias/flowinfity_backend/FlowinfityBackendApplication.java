4b1ef43599d90bf656f714c8f788a1146a3064a0
snoopyxpl <chaigneaunicolas25@gmail.com>
2024-05-09 13:27:53 +0200

    first commit

---
+++ src/main/java/com/dahlias/flowinfity_backend/FlowinfityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dahlias.flowinfity_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowinfityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowinfityBackendApplication.class, args);
+	}
+
+}
