9d552f13d5913fe5fa0628230654b00752eb6d9b
snoopyxpl <chaigneaunicolas25@gmail.com>
2024-05-11 14:14:15 +0200

    bug fix

---
+++ src/main/java/com/dahlias/flowinfity_backend/util/AssociationUtil.java
@@ -0,0 +1,24 @@
+package com.dahlias.flowinfity_backend.util;
+
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.dahlias.flowinfity_backend.data.Association;
+import com.dahlias.flowinfity_backend.repository.AssociationRepository;
+
+@Component
+public class AssociationUtil {
+
+    private static AssociationRepository associationRepository;
+
+    @Autowired
+    public void setAssociationRepository(AssociationRepository associationRepository) {
+        AssociationUtil.associationRepository = associationRepository;
+    }
+
+    public static void updateAssociationMoney(Association association, BigDecimal amount) {
+        association.setMoney(association.getMoney().subtract(amount));
+        associationRepository.save(association);
+    }
+}
